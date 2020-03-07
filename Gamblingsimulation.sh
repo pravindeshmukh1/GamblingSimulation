6e08a235851a80eed6906bc098d69e44e380cf8e
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-07 18:15:00 +0530

    ADDED:Check the win and loose

--- Gamblingsimulation.sh
+++ Gamblingsimulation.sh
@@ -4,13 +4,23 @@ echo "Welcome to Gambling Simulation"
 
 STAKE=100
 BET=1
+CASH=$STAKE
+MAX_STAKE=$((STAKE+STAKE/2))
+MIN_STAKE=$((MAX_STAKE-STAKE))
+echo $MAX_STAKE $MIN_STAKE
 
 function gambler() {
-	if [[ $((RANDOM%2)) -eq 1 ]]
-	then
-		echo "win"
-	else
-		echo "loose"
-	fi
+	while [[ $CASH -lt $MAX_STAKE  && $CASH -gt $MIX_STAKE ]]
+	do
+		if [[ $((RANDOM%2)) -eq 1 ]]
+		then
+			echo "win"
+				CASH=$((CASH+BET))
+		else
+			echo "loose"
+				CASH=$((CASH-BET))
+		fi
+	done
 }
 gambler
+echo "$CASH"
