655465bca53cbccaeba584f454c5370ebbe03cce
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-07 19:02:37 +0530

    ADDED:Identify Win and Loose and Total Amount

--- Gamblingsimulation.sh
+++ Gamblingsimulation.sh
@@ -2,25 +2,39 @@
 
 echo "Welcome to Gambling Simulation"
 
+#constant
 STAKE=100
 BET=1
 CASH=$STAKE
+
 MAX_STAKE=$((STAKE+STAKE/2))
 MIN_STAKE=$((MAX_STAKE-STAKE))
-echo $MAX_STAKE $MIN_STAKE
 
-function gambler() {
+function checkWinLoose() {
 	while [[ $CASH -lt $MAX_STAKE  && $CASH -gt $MIX_STAKE ]]
 	do
 		if [[ $((RANDOM%2)) -eq 1 ]]
 		then
-			echo "win"
 				CASH=$((CASH+BET))
 		else
-			echo "loose"
 				CASH=$((CASH-BET))
 		fi
 	done
+		totalCash=$((CASH-STAKE))
+		echo $totalCash
 }
-gambler
-echo "$CASH"
+
+function getTotalAmount() {
+ 	for (( day=1;day<=20;day++ ))
+	do
+		totalAmount=$((totalAmount + $(checkWinLoose)))
+	done
+
+	if [[ $totalAmount -gt 0 ]]
+	then
+		echo "Total Amount win" $totalAmount
+	else
+		echo "Total Amount Loose" $totalAmount
+	fi
+}
+getTotalAmount
